567f0ae2fd83f1d2647f4be0a17ec78dfc462108
Gregor Moenke <gregor.moenke@esi-frankfurt.de>
2022-03-03 10:50:58 +0000

    Update test_runner_cs.py

--- test_runner_cs.py
+++ test_runner_cs.py
@@ -6,10 +6,14 @@ mat_file_dir = '/cs/scratch/syncopy/MAT-Files'
 mat_name = 'matdata-v73.mat'
 fname = os.path.join(mat_file_dir, mat_name)
 
-def test_cs_access():
+def test_os_access():
     print(f'os.path finds {mat_file_dir} - {os.path.exists(mat_file_dir)}')
     print(f'os.path finds {fname} - {os.path.exists(fname)}')
     assert os.path.exists(mat_file_dir)
     assert os.path.exists(fname)
+
+def test_spyio_parser():
     io_parser(fname, isfile=True)
+def test_spyio_parser_has_to_fail():
+    io_parser('nixda', isfile=True)
     
